0aed965e5641964e39f5f0fc1d3e9a9d2976e638
LockVision <hassanelmaayati@gmail.com>
2026-09-23 21:14:54 +0300

    .

--- exercises.py
+++ exercises.py
@@ -45,29 +45,36 @@ print("---------------------------")
 # Create a tuple named foods containing the same number of foods (strings) as there are names in the students list.
 # Create a variable named meal and assign an empty string to it.
 # Use a for loop to iterate over the strings in foods and append each string to meal.
-foods=('Double cheese B burger ','Mashwiyat mix ','Little caesars pizza')
+foods = ("Double cheese B burger ", "Mashwiyat mix ", "Little caesars pizza")
+
+
 def combine_foods():
     # your code here
-    meal=""
+    meal = ""
     for food in foods:
-      meal= meal+food+''
+        meal = meal + food + ""
     return meal
-    
+
+
 # Call the function and print the result
-print('Exercise 2:', combine_foods())
+print("Exercise 2:", combine_foods())
 print("---------------------------")
 
 # Exercise 3: Slicing Tuples
 #
 # Using the slice operator, assign a new tuple containing only the last two food strings in the foods to a variable named last_two_foods.
 
+
 def slice_foods():
     # your code here
-    last_two_foods=foods[-2:]
+    last_two_foods = foods[-2:]
     return last_two_foods
+
+
 # Call the function and print the result
-print('Exercise 3:', slice_foods())
+print("Exercise 3:", slice_foods())
 print("---------------------------")
+
 
 # Exercise 4: Dictionaries and String Formatting
 #
@@ -75,15 +82,17 @@ print("---------------------------")
 # Using the home_town dictionary, assign to a variable named home_town_message a string with this format: “I was born in <city>, <state> - population of <population>”
 def hometown_info():
     # your code here
-    home_town={
-      'city':  'riffa',
-      'state':  'south',
-      'population'  :'60k',
+    home_town = {
+        "city": "riffa",
+        "state": "south",
+        "population": "60k",
     }
-    home_town_message=f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}"
+    home_town_message = f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}"
     return home_town_message
+
+
 # Call the function and print the result
-print('Exercise 4:', hometown_info())
+print("Exercise 4:", hometown_info())
 print("---------------------------")
 
 # Exercise 5: Iterating Over Dictionary Items
@@ -91,9 +100,21 @@ print("---------------------------")
 # Define an empty list named home_town_items.
 # Use a for loop to iterate over the key: value pairs in the home_town dictionary and append a string with the following format to home_town_items: "<key> = <value>"
 
+
 def list_home_town_items():
     # your code here
+    home_town = {
+        "city": "riffa",
+        "state": "south",
+        "population": "60k",
+    }
+    home_town_items = []
+    
+    for key, value in home_town_items():
+          home_town_items.append(f"{key} = {value}")
+    return home_town_items
+
 
 # Call the function and print the result
-print('Exercise 5:', list_home_town_items())
+print("Exercise 5:", list_home_town_items())
 print("---------------------------")
