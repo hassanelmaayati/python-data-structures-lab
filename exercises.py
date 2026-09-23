fb6f17bb05b3b61d0b1db1f73bba684b6f486f7b
LockVision <hassanelmaayati@gmail.com>
2026-09-23 20:40:42 +0300

    .

--- exercises.py
+++ exercises.py
@@ -73,29 +73,27 @@ print("---------------------------")
 #
 # Create a dictionary named home_town containing the keys of city, state, and population.
 # Using the home_town dictionary, assign to a variable named home_town_message a string with this format: “I was born in <city>, <state> - population of <population>”
+def hometown_info():
+    # your code here
+    home_town={
+      'city':  'riffa',
+      'state':  'south',
+      'population'  :'60k',
+    }
+    home_town_message=f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}"
+    return home_town_message
+# Call the function and print the result
+print('Exercise 4:', hometown_info())
+print("---------------------------")
 
+# Exercise 5: Iterating Over Dictionary Items
+#
+# Define an empty list named home_town_items.
+# Use a for loop to iterate over the key: value pairs in the home_town dictionary and append a string with the following format to home_town_items: "<key> = <value>"
 
+def list_home_town_items():
+    # your code here
 
-
-
-# 8999999999999999999999999999999999999999999999999999999999999999
-
-
-# def hometown_info():
-#     # your code here
-
-# # Call the function and print the result
-# print('Exercise 4:', hometown_info())
-# print("---------------------------")
-
-# # Exercise 5: Iterating Over Dictionary Items
-# #
-# # Define an empty list named home_town_items.
-# # Use a for loop to iterate over the key: value pairs in the home_town dictionary and append a string with the following format to home_town_items: "<key> = <value>"
-
-# def list_home_town_items():
-#     # your code here
-
-# # Call the function and print the result
-# print('Exercise 5:', list_home_town_items())
-# print("---------------------------")
+# Call the function and print the result
+print('Exercise 5:', list_home_town_items())
+print("---------------------------")
